55557d6ee33294f51dc080ac93e81d67d74acf1d
Thomas Ziemek <tziemek@artiso.com>
2017-10-31 00:34:43 +0100

    add data entities

---
+++ WeeklyCurriculum.Wpf/Data/SchoolClassData.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyCurriculum.Wpf.Data
+{
+    public class SchoolClassData
+    {
+        public string Name { get; set; }
+        public bool IsMonday { get; set; }
+        public bool IsTuesday { get; set; }
+        public bool IsWednesday { get; set; }
+        public bool IsThursday { get; set; }
+        public bool IsFriday { get; set; }
+        public List<SchoolWeekData> SchoolWeeks {get;set;}
+    }
+}
---
+++ WeeklyCurriculum.Wpf/Data/SchoolWeekData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyCurriculum.Wpf.Data
+{
+    public class SchoolWeekData
+    {
+        public int Year { get; set; }
+        public int Week { get; set; }
+        public string Monday { get; set; }
+        public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+        public string Thursday { get; set; }
+        public string Friday { get; set; }
+    }
+}
